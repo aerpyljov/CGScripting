e6557d40509106e8cf6acbea47f952e802a1751c
Alexey Erpyljov <aerpyljov@gmail.com>
2017-03-12 13:21:10 +0300

    Additional materials - working with ZIP-files re-written

--- 5. Additional materials/zip_example.py
+++ 5. Additional materials/zip_example.py
@@ -1,33 +1,49 @@
-from zipfile import ZipFile
+"""
+In order to test the script, run it in a folder with the following test data:
+- Folder 'To be packed', containing a file 'file.txt' with some text and any other subfolders and files.
+
+Advice for real life use:
+- When you create a ZIP-archive, check, that the archive isn't in a folder to be packed.
+"""
+
+
+import zipfile
 import os
 
-src = 'C:/tmp/data/file.txt'
-zFile = 'C:/tmp/archive.zip'
-# zf = ZipFile(zFile, 'w')
-# zf.write(src, os.path.basename(src))
-# zf.close()
+"""How to create a ZIP-archive from a file"""
+scriptFolder = os.path.dirname(__file__)
+srcFolder = os.path.join(scriptFolder, 'To be packed')
+srcFile = os.path.join(srcFolder, 'file.txt')
+zFile = os.path.join(scriptFolder, 'archive.zip')
 
-trg = 'C:/tmp'
-zf = ZipFile(zFile, 'r')
+zf = zipfile.ZipFile(zFile, 'w', compression=zipfile.ZIP_DEFLATED, allowZip64=True)
+# Compressed archive, archives larger than 2GB allowed
+
+zf.write(srcFile, os.path.basename(srcFile))    # File and its name in the archive
+zf.close()
+
+
+"""How to create a ZIP-archive from a folder"""
+zf = zipfile.ZipFile(zFile, 'w', compression=zipfile.ZIP_DEFLATED, allowZip64=True)
+for path, dirs, files in os.walk(srcFolder):
+    for f in files:
+        fullPath = os.path.join(path, f)
+        relativePath = os.path.relpath(fullPath, srcFolder)
+        zf.write(fullPath, relativePath)    # File and its name in the archive
+zf.close()
+
+
+"""How to unpack a ZIP-archive into a target folder"""
+trg = os.path.join(scriptFolder, 'Unpacked')
+zf = zipfile.ZipFile(zFile, 'r')
 for f in zf.namelist():
     full = os.path.join(trg, f)
     d = os.path.dirname(full)
-    if d:
+    if d:   # For folders in the ZIP-archive
         if not os.path.exists(d):
             os.makedirs(d)
-    if os.path.basename(f):
+    if os.path.basename(f):   # For files in the ZIP-archive
         out = open(full, 'wb')
         out.write(zf.read(f))
         out.close()
 zf.close()
-
-folder = 'c:/tmp'
-zf = ZipFile('c:/tmp/archive.zip', 'w')
-
-for path, dirs, files in os.walk(folder):
-    for file in files:
-        full = os.path.join(path, file)
-        zf.write(full)
-zf.close()
-
-os.path.splitext('c:/file.txt')
