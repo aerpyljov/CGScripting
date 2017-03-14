e3f8a7868d7a9f0f328cc29e35c87b4bbee01559
Alexey Erpyljov <aerpyljov@gmail.com>
2017-03-14 20:16:05 +0300

    PySide basics - simpleWidget amended

--- 6. PySide basics/simpleWidget.py
+++ 6. PySide basics/simpleWidget.py
@@ -1,12 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
 from PySide.QtGui import *
 
+
+"Create an application"
 app = QApplication([])
-w = QWidget()
-l = QHBoxLayout()
-w.setLayout(l)
-label = QLabel('Text')
-l.addWidget(label)
-b = QPushButton('OK')
-l.addWidget(b)
-w.show()
+
+"Create a widget - an empty window"
+widget = QWidget()
+
+"All labels, buttons, etc. must be put on a layout"
+layout = QHBoxLayout()  # Horizontal layout
+widget.setLayout(layout)
+
+label = QLabel('Кнопка ничего не делает:')
+layout.addWidget(label)
+button = QPushButton('Проверить')
+layout.addWidget(button)
+
+"Make the widget visible"
+widget.show()
+
+"Run the application"
 app.exec_()
