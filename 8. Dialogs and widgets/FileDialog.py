fcdb2cd4018ec4cead43e671e4ce853500a9c313
Alexey Erpyljov <aerpyljov@gmail.com>
2017-04-20 19:50:15 +0300

    Dialogs and widgets - FileDialog added

---
+++ 8. Dialogs and widgets/FileDialog.py
@@ -0,0 +1,23 @@
+from PySide.QtGui import *
+# from PyQt4.QtGui import *
+
+class simpleWindow(QWidget):
+    def __init__(self):
+        super(simpleWindow, self).__init__()
+        ly = QVBoxLayout(self)
+        self.btn  = QPushButton('Open')
+        ly.addWidget(self.btn)
+        self.resize(300,200)
+        self.btn.clicked.connect(self.showMessage)
+
+    def showMessage(self):
+        _filter = 'Python File(*.py);; All(*.*)'
+        d = QFileDialog.getOpenFileName(self, 'Set folder', 'c:/tmp', _filter)
+        print d
+
+if __name__ == '__main__':
+    app = QApplication([])
+    w = simpleWindow()
+    w.show()
+    app.exec_()
+    
--- 8. Dialogs and widgets/README.md
+++ 8. Dialogs and widgets/README.md
@@ -1,7 +1,7 @@
 All scripts are provided as parts of the course, but I amended them a bit and added some comments.
 
 - "InputDialog.py" - a simple dialog window based on QInputDialog.
-
+- "FileDialog.py" - a simple dialog window based on QFileDialog.
 
 
 
