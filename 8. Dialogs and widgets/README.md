feac650a5e9209447a3fd47f00c30d4781709b2d
Alexey Erpyljov <aerpyljov@gmail.com>
2017-04-27 20:25:08 +0300

    Dialogs and widgets - table_widget commented

--- 8. Dialogs and widgets/README.md
+++ 8. Dialogs and widgets/README.md
@@ -5,7 +5,7 @@ All scripts are provided as parts of the course, but I amended them a bit and ad
 - "CustomDialog" - a user-defined dialog window based on QDialog.
 - "list_widget.py" - show a list of files in the current folder, display the text of the selected file and open the double-clicked file.
 - "table_widget.py" - show files in the current folder as a table with file names and sizes.
-
+- "tree_widget.py" - show the parent folder with subfolders and files, react if file names are being changed.
 
 NB: Pay attention, that you need to install PySide first:
 http://wiki.qt.io/PySideDownloads
--- 8. Dialogs and widgets/tree_widget.py
+++ 8. Dialogs and widgets/tree_widget.py
@@ -1,7 +1,9 @@
 from PySide.QtGui import *
 from PySide.QtCore import *
 import os
-path = os.path.dirname(os.path.dirname(__file__))
+path = os.path.dirname(os.path.dirname(__file__))   # Get the parent folder of the folder with yhe script
+
+
 class simpleWindow(QWidget):
     def __init__(self):
         super(simpleWindow, self).__init__()
@@ -16,7 +18,7 @@ class simpleWindow(QWidget):
         self.resize(500,400)
         self.updateTree()
 
-    def updateTree(self):
+    def updateTree(self):    # Do not react when nodes created initially, not modified by user in GUI
         self.tree.blockSignals(True)
         self.fillTree()
         self.tree.blockSignals(False)
@@ -27,17 +29,17 @@ class simpleWindow(QWidget):
         if not root:
             root = path
         for f in os.listdir(root):
-            if f[0] in ['.', '_']: continue
+            if f[0] in ['.', '_']: continue    # Do not include files which names start with these symbols
             item = QTreeWidgetItem()
             item.setText(0, f)
             parent.addChild(item)
             fullpath = os.path.join(root, f)
-            if os.path.isdir(fullpath):
+            if os.path.isdir(fullpath):    # Open all folder, but not files
                 self.fillTree(item, fullpath)
-                item.setExpanded(1)
-            else:
+                item.setExpanded(1)    # Expand the node in the tree
+            else:    # For nodes that are files, not folders
                 item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable)
-                item.setData(0, Qt.UserRole, {'path':os.path.normpath(fullpath)})
+                item.setData(0, Qt.UserRole, {'path': os.path.normpath(fullpath)})    # UserRole is a not reserved kind of data; the last argument creates a dict, not a string
 
     def action(self,item):
         print item.text(0)
@@ -45,10 +47,8 @@ class simpleWindow(QWidget):
         print s
 
 
-
 if __name__ == '__main__':
     app = QApplication([])
     w = simpleWindow()
     w.show()
     app.exec_()
-    
