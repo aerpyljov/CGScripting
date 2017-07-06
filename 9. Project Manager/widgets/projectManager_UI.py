84dd83e4a749a489269be47ec17b69bb17a5003b
Alexey Erpyljov <aerpyljov@gmail.com>
2017-07-06 20:37:38 +0300

    Project Manager - GUI modified, new buttons added

--- 9. Project Manager/widgets/projectManager_UI.py
+++ 9. Project Manager/widgets/projectManager_UI.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'C:\Users\Alexey\Documents\GitHub\CGScripting\9. Project Manager\widgets\projectManager.ui'
 #
-# Created: Thu May 04 20:16:54 2017
+# Created: Thu Jul 06 20:35:51 2017
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,7 +12,7 @@ from PySide import QtCore, QtGui
 class Ui_projectManager(object):
     def setupUi(self, projectManager):
         projectManager.setObjectName("projectManager")
-        projectManager.resize(800, 600)
+        projectManager.resize(800, 593)
         self.centralwidget = QtGui.QWidget(projectManager)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.centralwidget)
@@ -28,18 +28,27 @@ class Ui_projectManager(object):
         self.splitter = QtGui.QSplitter(self.splitter_2)
         self.splitter.setOrientation(QtCore.Qt.Horizontal)
         self.splitter.setObjectName("splitter")
-        self.widget = QtGui.QWidget(self.splitter)
-        self.widget.setObjectName("widget")
-        self.verticalLayout = QtGui.QVBoxLayout(self.widget)
+        self.layoutWidget = QtGui.QWidget(self.splitter)
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.verticalLayout = QtGui.QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.create_btn = QtGui.QPushButton(self.widget)
+        self.create_btn = QtGui.QPushButton(self.layoutWidget)
         self.create_btn.setObjectName("create_btn")
         self.verticalLayout.addWidget(self.create_btn)
-        self.templateEditor_btn = QtGui.QPushButton(self.widget)
+        self.update_btn = QtGui.QPushButton(self.layoutWidget)
+        self.update_btn.setObjectName("update_btn")
+        self.verticalLayout.addWidget(self.update_btn)
+        self.backup_btn = QtGui.QPushButton(self.layoutWidget)
+        self.backup_btn.setObjectName("backup_btn")
+        self.verticalLayout.addWidget(self.backup_btn)
+        self.archive_btn = QtGui.QPushButton(self.layoutWidget)
+        self.archive_btn.setObjectName("archive_btn")
+        self.verticalLayout.addWidget(self.archive_btn)
+        self.templateEditor_btn = QtGui.QPushButton(self.layoutWidget)
         self.templateEditor_btn.setObjectName("templateEditor_btn")
         self.verticalLayout.addWidget(self.templateEditor_btn)
-        self.info_gb = QtGui.QGroupBox(self.widget)
+        self.info_gb = QtGui.QGroupBox(self.layoutWidget)
         self.info_gb.setObjectName("info_gb")
         self.verticalLayout_3 = QtGui.QVBoxLayout(self.info_gb)
         self.verticalLayout_3.setObjectName("verticalLayout_3")
@@ -48,7 +57,10 @@ class Ui_projectManager(object):
         self.info_lb.setObjectName("info_lb")
         self.verticalLayout_3.addWidget(self.info_lb)
         self.verticalLayout.addWidget(self.info_gb)
-        self.settings_btn = QtGui.QPushButton(self.widget)
+        self.refresh_btn = QtGui.QPushButton(self.layoutWidget)
+        self.refresh_btn.setObjectName("refresh_btn")
+        self.verticalLayout.addWidget(self.refresh_btn)
+        self.settings_btn = QtGui.QPushButton(self.layoutWidget)
         self.settings_btn.setObjectName("settings_btn")
         self.verticalLayout.addWidget(self.settings_btn)
         self.verticalLayout_2.addWidget(self.splitter_2)
@@ -60,8 +72,12 @@ class Ui_projectManager(object):
     def retranslateUi(self, projectManager):
         projectManager.setWindowTitle(QtGui.QApplication.translate("projectManager", "Project Manager", None, QtGui.QApplication.UnicodeUTF8))
         self.create_btn.setText(QtGui.QApplication.translate("projectManager", "Create Project", None, QtGui.QApplication.UnicodeUTF8))
+        self.update_btn.setText(QtGui.QApplication.translate("projectManager", "Update Project", None, QtGui.QApplication.UnicodeUTF8))
+        self.backup_btn.setText(QtGui.QApplication.translate("projectManager", "Move to BACKUP", None, QtGui.QApplication.UnicodeUTF8))
+        self.archive_btn.setText(QtGui.QApplication.translate("projectManager", "Move to ARCHIVE", None, QtGui.QApplication.UnicodeUTF8))
         self.templateEditor_btn.setText(QtGui.QApplication.translate("projectManager", "Template Editor", None, QtGui.QApplication.UnicodeUTF8))
         self.info_gb.setTitle(QtGui.QApplication.translate("projectManager", "Info", None, QtGui.QApplication.UnicodeUTF8))
         self.info_lb.setText(QtGui.QApplication.translate("projectManager", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
+        self.refresh_btn.setText(QtGui.QApplication.translate("projectManager", "Refresh", None, QtGui.QApplication.UnicodeUTF8))
         self.settings_btn.setText(QtGui.QApplication.translate("projectManager", "Settings", None, QtGui.QApplication.UnicodeUTF8))
 
