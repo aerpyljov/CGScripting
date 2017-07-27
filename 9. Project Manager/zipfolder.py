ef96e6d241f4f607be15611209613a08d4d02505
Alexey Erpyljov <aerpyljov@gmail.com>
2017-07-27 20:55:40 +0300

    Project Manager - minor changes

--- 9. Project Manager/zipfolder.py
+++ 9. Project Manager/zipfolder.py
@@ -7,13 +7,12 @@ import settings
 
 def zipFolder(old_path, new_path):
     if sys.platform == 'win32':
-        winEncoding = settings.SettingsClass().load()['winEncoding']
+        win_encoding = settings.SettingsClass().load()['winEncoding']
         old_path = old_path
         archiver_folder = os.path.dirname(os.path.abspath(__file__))
         archiver = os.path.join(archiver_folder, '7zr.exe')
         archive_name = (new_path + '.7z')
-        # subprocess.call([archiver, 'a', '-bd', '-y', archive_name, old_path])
         command = r'"{0}" a -bd -y "{1}" "{2}"'.format(archiver, archive_name, old_path)
-        subprocess.call(command.encode(winEncoding))
+        subprocess.call(command.encode(win_encoding))
     else:
         shutil.make_archive(new_path, 'zip', old_path)
