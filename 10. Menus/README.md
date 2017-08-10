04737a52636271a3970700f7666c173165a3d618
Alexey Erpyljov <aerpyljov@gmail.com>
2017-08-10 19:36:03 +0300

    Menus - a context menu added

--- 10. Menus/README.md
+++ 10. Menus/README.md
@@ -1,7 +1,7 @@
 All scripts are provided as parts of the course, but I amended them a bit and added some comments.
 
 - "windowMenu.py" - a menu for QMainWindow.
-
+- "widgetMenu.py" - a context menu for any widget, for example QLineEdit.
 
 
 
---
+++ 10. Menus/widgetMenu.py
@@ -0,0 +1,36 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+textArray = 'Click', 'Press', 'Enter'
+
+class widgetMenuClass(QWidget):
+    def __init__(self):
+        super(widgetMenuClass, self).__init__()
+        ly = QVBoxLayout(self)
+        self.btn = QPushButton('Click')
+        ly.addWidget(self.btn)
+        self.line = QLineEdit()
+        ly.addWidget(self.line)
+
+        self.line.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.line.customContextMenuRequested.connect(self.openMenu)
+
+    def openMenu(self, pos):
+        pos = self.sender().mapToGlobal(pos)
+        try:
+            menu = self.sender().createStandardContextMenu()
+        except:
+            menu = QMenu()
+        for i in textArray:
+            menu.addAction( QAction(i, self) )
+        a = menu.exec_(pos)
+        if a:
+            self.sender().setText(a.text())
+
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    w = widgetMenuClass()
+    w.show()
+    app.exec_()
