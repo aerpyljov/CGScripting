a6b6105f35e3e460ce9c03e0b7259ec53f58fe1a
Alexey Erpyljov <aerpyljov@gmail.com>
2017-09-07 21:01:02 +0300

    Drag&Drop: README and comments added

---
+++ 12. Drag&Drop/README.md
@@ -0,0 +1,15 @@
+The script is provided as a part of the course, but I amended it a bit and added some comments.
+
+The widget demonstrates how drag & drop works.
+Its features:  
+- Drag files with the left mouse button and drop them on the widget: they'll be displayed in a list.
+- Select rows in the list and drag them with the left mouse button: nothing will happen. Only the middle button works.
+- Select rows in the list, drag them and drop in a folder: you'll see the standard menu for drag'n'dropping files between folders.
+- Repeat, but drop on another copy of the widget: the rows will move.
+- Repeat, but drop on the same widget or any other place: nothing will happen.
+- A custom icon is displayed near the cursor when you drag rows from the widget.
+
+
+
+NB: Pay attention, that you need to install PySide first:
+http://wiki.qt.io/PySideDownloads
--- 12. Drag&Drop/dnd_widget.py
+++ 12. Drag&Drop/dnd_widget.py
@@ -14,13 +14,15 @@ class listWidgetClass(QListWidget):
         self.files = []
 
     def dropEvent(self, event):
+        """On drop add new rows, only if files are dragging"""
         mimedata = event.mimeData()
         if mimedata.hasUrls():
             for f in mimedata.urls():
-                self.addFile(f.toLocalFile())
+                self.addFile(f.toLocalFile())   # Call with path, not QUrl
 
     def dragEnterEvent(self, event):
-        if event.source() is self:
+        """Called when the cursor gets to the area first"""
+        if event.source() is self:  # Forbid dropping on the same widget
             event.ignore()
         else:
             mimedata = event.mimeData()
@@ -30,52 +32,58 @@ class listWidgetClass(QListWidget):
                 event.ignore()
 
     def dragMoveEvent(self, event):
-        if event.source() is self:
+        """Called many times while the cursor moving"""
+        if event.source() is self:  # Forbid dropping on the same widget
             event.ignore()
         else:
             mimedata = event.mimeData()
-            if mimedata.hasUrls():
+            if mimedata.hasUrls():  # How does the cursor look?
                 event.accept()
             else:
                 event.ignore()
 
     def startDrag(self, dropAction):
+        """Create MIME data when dragging and add an icon to the cursor"""
         drag = QDrag(self)
         mimedata = QMimeData()
         url = []
         for i in self.selectedItems():
             url.append(i.data(Qt.UserRole))
-        mimedata.setUrls([QUrl.fromLocalFile(x) for x in url])
+        mimedata.setUrls([QUrl.fromLocalFile(x) for x in url])  # Paths to QUrls
         drag.setMimeData(mimedata)
-        pix = QPixmap(icon)
+        pix = QPixmap(icon) # Custom icon near the cursor
         drag.setPixmap(pix)
         r = drag.exec_()
-        if r == Qt.DropAction.MoveAction:
+        if r == Qt.DropAction.MoveAction:   # Only if dropped where allowed
             self.deleteSelected()
 
     def addFile(self, path):
-        if not path in self.files:
+        """Add new rows in the list"""
+        if not path in self.files:  # Don't add the same file twice
             item = QListWidgetItem(self)
             item.setText(os.path.basename(path))
             item.setData(Qt.UserRole, path)
             self.files.append(path)
 
     def deleteSelected(self):
+        """Delete rows from the original widget after dropping on another widget"""
         for s in self.selectedItems():
             self.files.remove(s.data(32))
             self.takeItem(self.indexFromItem(s).row())
 
     def mousePressEvent(self, event):
+        """Forbid dragging by the left mouse button"""
         if event.button() == Qt.MouseButton.RightButton:
             pass
         elif event.button() == Qt.MouseButton.LeftButton:
             self.setDragDropMode(QAbstractItemView.NoDragDrop)
-            super(listWidgetClass, self).mousePressEvent(event)
+            super(listWidgetClass, self).mousePressEvent(event) # Standard effect allowed
         else:
             self.setDragDropMode(QAbstractItemView.DragDrop)
             super(listWidgetClass, self).mousePressEvent(event)
 
     def mouseReleaseEvent(self, event):
+        """Restore DragDrop mode after the left mouse button released"""
         self.setDragDropMode(QAbstractItemView.DragDrop)
         super(listWidgetClass, self).mouseReleaseEvent(event)
 
