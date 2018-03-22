b1bb849218fbfd7aa514b5f1c1e1093934b2c9ee
Alexey Erpyljov <aerpyljov@gmail.com>
2018-03-22 19:42:54 +0300

    Amended script for compilation Qt Designer files

--- 6. PySide basics/compile_ui/compile2.bat
+++ 6. PySide basics/compile_ui/compile2.bat
@@ -1,8 +1,10 @@
 set UIFILE=%1
 set UIDIR=%~dp$PATH:1
 set FILENAME=%~n1
-set NNAME=%UIDIR%%FILENAME%_UI.py
-set SNAME=%UIDIR%%FILENAME%_UIs.py
+set NNAME=%UIDIR%%FILENAME%_UI_pyqt.py
+set SNAME=%UIDIR%%FILENAME%_UI.py
 
 CALL C:\Python27\Lib\site-packages\PyQt4\pyuic4.bat %UIFILE% -o "%NNAME%"
 CALL C:\Python27\Scripts\pyside-uic.exe %UIFILE% -o "%SNAME%"
+
+set /p Done="Press any key to exit"
