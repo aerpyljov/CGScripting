8428a0101546c3c7d85001e4787c2a2c17eec5dd
Alexey Erpyljov <aerpyljov@gmail.com>
2018-06-21 20:42:32 +0300

    Spline Ramp - Points can be dragged

--- 15. Custom Widget/splineRamp/splineRamp.py
+++ 15. Custom Widget/splineRamp/splineRamp.py
@@ -8,10 +8,22 @@ class splineRampWidget(QWidget):
         self.resize(500, 300)
 
         self.lineWidth = 3
-        self.pointSize = 30
+        self.pointSize = 10
 
         self.point1 = QPoint(0, 10)
         self.point2 = QPoint(500, 250)
+        self.dragged = None  # Either point1 or point2 to be moved
+
+        self.region1 = QRect()
+        self.region2 = QRect()
+        self.regionSize = 40
+        self.updateRegions()
+
+    def updateRegions(self):
+        self.region1 = QRect(0, 0, self.regionSize, self.regionSize)
+        self.region1.moveCenter(self.point1)
+        self.region2 = QRect(0, 0, self.regionSize, self.regionSize)
+        self.region2.moveCenter(self.point2)
 
 
     def paintEvent(self, event):
@@ -35,7 +47,30 @@ class splineRampWidget(QWidget):
         painter.drawEllipse(self.point1, self.pointSize, self.pointSize)
         painter.drawEllipse(self.point2, self.pointSize, self.pointSize)
 
+        painter.setPen(QPen(QBrush(Qt.white), 1))
+        painter.setBrush(Qt.NoBrush)
+        painter.drawRect(self.region1)
+        painter.drawRect(self.region2)
         painter.end()
+
+    def mousePressEvent(self, event):
+        if (self.region1.contains(event.pos())):
+            self.dragged = self.point1
+        elif (self.region2.contains(event.pos())):
+            self.dragged = self.point2
+        super(splineRampWidget, self).mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if self.dragged:
+            self.dragged.setY(event.pos().y())
+            self.update()
+        super(splineRampWidget, self).mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        self.dragged = None
+        self.updateRegions()
+        self.update()
+        super(splineRampWidget, self).mouseReleaseEvent(event)
 
 
 if __name__ == '__main__':
