2cc1752ae0f139da883224dad32a8d4e506c53bc
Alexey Erpyljov <aerpyljov@gmail.com>
2018-06-28 20:03:31 +0300

    Spline Ramp - Points don't leave the widget

--- 15. Custom Widget/splineRamp/splineRamp.py
+++ 15. Custom Widget/splineRamp/splineRamp.py
@@ -10,9 +10,12 @@ class splineRampWidget(QWidget):
         self.lineWidth = 3
         self.pointSize = 10
 
-        self.point1 = QPoint(0, 10)
-        self.point2 = QPoint(500, 250)
+        self.point1 = QPointF(0, 10)
+        self.point2 = QPointF(500, 250)
         self.dragged = None  # Either point1 or point2 to be moved
+
+        self.factor1 = 0.0
+        self.factor2 = 1.0
 
         self.region1 = QRect()
         self.region2 = QRect()
@@ -21,9 +24,12 @@ class splineRampWidget(QWidget):
 
     def updateRegions(self):
         self.region1 = QRect(0, 0, self.regionSize, self.regionSize)
-        self.region1.moveCenter(self.point1)
+        self.region1.moveCenter(self.point1.toPoint())  # Works with QPoint, not QPointF
         self.region2 = QRect(0, 0, self.regionSize, self.regionSize)
-        self.region2.moveCenter(self.point2)
+        self.region2.moveCenter(self.point2.toPoint())
+
+        self.factor1 = self.point1.y() / float(self.size().height())
+        self.factor2 = self.point2.y() / float(self.size().height())
 
 
     def paintEvent(self, event):
@@ -47,10 +53,12 @@ class splineRampWidget(QWidget):
         painter.drawEllipse(self.point1, self.pointSize, self.pointSize)
         painter.drawEllipse(self.point2, self.pointSize, self.pointSize)
 
-        painter.setPen(QPen(QBrush(Qt.white), 1))
-        painter.setBrush(Qt.NoBrush)
-        painter.drawRect(self.region1)
-        painter.drawRect(self.region2)
+        # Paint the regions for drag'n'drop
+        # painter.setPen(QPen(QBrush(Qt.white), 1))
+        # painter.setBrush(Qt.NoBrush)
+        # painter.drawRect(self.region1)
+        # painter.drawRect(self.region2)
+
         painter.end()
 
     def mousePressEvent(self, event):
@@ -61,8 +69,10 @@ class splineRampWidget(QWidget):
         super(splineRampWidget, self).mousePressEvent(event)
 
     def mouseMoveEvent(self, event):
-        if self.dragged:
-            self.dragged.setY(event.pos().y())
+        if not self.dragged is None:  # Zero is a valid value
+            y = event.pos().y()
+            s = self.size()
+            self.dragged.setY(min(max(y, 1), s.height()))  # Prevent moving outside the widget
             self.update()
         super(splineRampWidget, self).mouseMoveEvent(event)
 
@@ -71,6 +81,15 @@ class splineRampWidget(QWidget):
         self.updateRegions()
         self.update()
         super(splineRampWidget, self).mouseReleaseEvent(event)
+
+    def resizeEvent(self, event):
+        self.point1.setY(event.size().height() * self.factor1)
+        self.point2.setY(event.size().height() * self.factor2)
+        self.point2.setX(event.size().width())
+        self.updateRegions()
+        self.update()
+        super(splineRampWidget, self).resizeEvent(event)
+
 
 
 if __name__ == '__main__':
